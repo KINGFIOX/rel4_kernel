af4a114503fe91630f7d2b7d6c70876366b52122
jackhu <jackhu.521.rose@gmail.com>
2024-07-09 13:21:12 +0800

    fix section bugs

--- sel4_vspace/src/arch/aarch64/interface.rs
+++ sel4_vspace/src/arch/aarch64/interface.rs
@@ -148,18 +148,17 @@ pub fn activate_kernel_window() {
     todo!()
 }
 
-pub fn unmap_page_table(asid: asid_t, vaddr: vptr_t, pt: &mut pte_t) {
+#[no_mangle]
+pub fn unmapPageTable(asid: asid_t, vaddr: vptr_t, pt: &mut pte_t) {
     pt.unmap_page_table(asid, vaddr);
 }
 
 #[no_mangle]
-#[link_section = ".boot.text"]
 pub fn create_unmapped_it_frame_cap(pptr: pptr_t, use_large: bool) -> cap_t {
     return create_it_frame_cap(pptr, 0, asidInvalid, use_large);
 }
 
 #[no_mangle]
-#[link_section = ".boot.text"]
 pub fn create_it_frame_cap(pptr: pptr_t, vptr: vptr_t, asid: asid_t, use_large: bool) -> cap_t {
     let mut frame_size = ARM_Small_Page;
     if use_large {
@@ -176,7 +175,6 @@ pub fn create_it_frame_cap(pptr: pptr_t, vptr: vptr_t, asid: asid_t, use_large: 
 }
 
 #[no_mangle]
-#[link_section = ".boot.text"]
 pub fn activate_kernel_vspace() {
     unsafe {
         clean_invalidate_l1_caches();
